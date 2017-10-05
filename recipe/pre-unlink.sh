6afa18cb6d732e015a1b642996c03c9e552777bc
Steven Silvester <steven.silvester@ieee.org>
2017-10-05 10:50:13 -0500

    Remove debug

--- recipe/pre-unlink.sh
+++ recipe/pre-unlink.sh
@@ -1,7 +1,4 @@
-
-
 server_extension="${PREFIX}/bin/jupyter-serverextension"
 if which $server_extension; then
-    echo "yup"
     ${server_extension} disable jupyterlab --py --sys-prefix > /dev/null 2>&1
 fi
